c703384adbcd3e9bacb14acba6a6109992f231d8
Nadia <nadeeaulia@gmail.com>
2024-07-25 14:32:35 +0700

    first

---
+++ models/index.js
@@ -0,0 +1,29 @@
+// models/index.js
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize('astaga', 'root', 'palemb2703', {
+    host: 'localhost',
+    dialect: 'mysql',
+});
+
+
+
+
+const User = require('./user')(sequelize, DataTypes);
+const Production = require('./production')(sequelize, DataTypes);
+const Report = require('./report')(sequelize, DataTypes);
+
+// Define associations
+User.associate && User.associate({ Report, Production });
+Production.associate && Production.associate({ Report });
+Report.associate && Report.associate({ Production, User });
+
+sequelize.sync({ alter: true })
+  .then(() => {
+    console.log('Database synchronized');
+  })
+  .catch(err => {
+    console.error('Error synchronizing database:', err);
+  });
+
+module.exports = { sequelize, User, Production, Report };
